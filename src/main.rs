7746b08f6bdac8614e4b9297d4bfd0efc9496e1d
andrew-pa <andrew.pa@outlook.com>
2021-07-06 07:55:13 -0700

    add cmd line opts documentation

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,40 @@ mod expander;
 
 #[derive(Clap)]
 enum Commands {
+    /// Preview a color palette
     Preview {
-        // Display different lightness variations of each color
+        /// Display different lightness variations of each color
         #[clap(long = "shades")]
         show_shades: bool
     },
+    /// Process configuration files and expand color expressions
     Expand {
-        // Path to expand. If this is a directory, then all files ending in `.uncol` will be
-        // expanded and the result will be placed besides them
+        /// Path to expand. If this is a directory, then all files ending in `.uncol` will be
+        /// expanded and the result will be placed besides them without the `.uncol` ending
         path: PathBuf
     },
+    /// Evaluate a color expression from the command line
     Eval {
-        // A color expression to evaluate
+        /// A color expression to evaluate
         expr: String,
+        /// Color the output with the result color
         #[clap(short = 'c', long = "colored")]
         colored: bool,
+        /// Format for output color:
+        /// '#' => sRGB hex,
+        /// '~' => Linear sRGB hex,
+        /// '$' => CSS RGB decimal,
+        /// '~' => CSS LCH decimal
         #[clap(short, default_value="#")]
         output_format: String
     }
 }
 
 #[derive(Clap)]
+#[clap(about = clap::crate_description!(), author = clap::crate_authors!(), version = clap::crate_version!())]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Options {
-    /// Specify palette file to use
+    /// Specify color palette file to use
     palette: PathBuf,
 
     #[clap(subcommand)]
